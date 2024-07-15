8b1f10cf29634f8894deaceec984f3a35df34d1f
Romain Billot <206266704@cegepsherbrooke.qc.ca>
2024-07-15 09:40:42 -0400

    exercice 1 - requis 1 pass

--- processor.js
+++ processor.js
@@ -1,13 +1,25 @@
 function processor(transmission) {
-    if (!transmission.includes("::")) {
-        throw new Error('Data is invalid ; should contain "::"');
-    }
-    let parts = transmission.split("::");
-    let rawData = parts[1];
-    return {
-        id: Number(parts[0]),
-        rawData: rawData
-    };
+	if (!transmission.includes("::")) {
+		throw new Error('Data is invalid ; should contain "::"');
+	}
+	let parts = transmission.split("::");
+	let rawData = parts[1];
+	validateRawDataFormat(rawData);
+
+	return {
+		id: Number(parts[0]),
+		rawData: rawData
+	};
+}
+
+function validateRawDataFormat(rawData) {
+	if (rawData[0] !== "<" && rawData[rawData.length - 1] !== ">") {
+		throw new Error('rawData is invalid ; should begin with "<" and end with ">"');
+	} else if (rawData[0] !== "<") {
+		throw new Error('rawData is invalid ; should begin with "<"');
+	} else if (rawData[rawData.length - 1] !== ">") {
+		throw new Error('rawData is invalid ; should end with ">"');
+	}
 }
 
 module.exports = processor;
