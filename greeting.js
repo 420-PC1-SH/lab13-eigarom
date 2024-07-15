27efe71cc7b474894489b8ef5bd7549effe30300
Romain Billot <206266704@cegepsherbrooke.qc.ca>
2024-07-15 10:35:54 -0400

    exercice 2 - requis 4 pass

--- greeting.js
+++ greeting.js
@@ -2,6 +2,10 @@ function greet(name) {
 	if (!name || name === "")
 		return "Hello there"
 
+	if (name instanceof Array && name.length === 2) {
+		return "Hello, " + name[0] + " and " + name[1];
+	}
+
 	if (name === name.toUpperCase())
 		return "HELLO " + name;
 
