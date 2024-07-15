1461af4033f19e5da5b545d88f4eaa0321fb6a9f
Romain Billot <206266704@cegepsherbrooke.qc.ca>
2024-07-15 10:18:15 -0400

    exercice 2 - requis 2 fail

--- greeting.test.js
+++ greeting.test.js
@@ -5,4 +5,21 @@ describe("test greeting ", function () {
 		let name = "Luke";
 		expect(greet(name)).toBe("Hello, Luke");
 	});
+
+	describe('empty name returns "Hello there"', function () {
+		test('undefined returns "Hello there"', () => {
+			let name;
+			expect(greet(name)).toBe("Hello there");
+		});
+
+		test('null returns "Hello there"', () => {
+			let name = null;
+			expect(greet(name)).toBe("Hello there");
+		});
+
+		test('empty string returns "Hello there"', () => {
+			let name = "";
+			expect(greet(name)).toBe("Hello there");
+		});
+	});
 });
