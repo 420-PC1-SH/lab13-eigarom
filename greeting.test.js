49202a5d2132d9ab7659a21872ad3006c824c128
Romain Billot <206266704@cegepsherbrooke.qc.ca>
2024-07-15 10:28:13 -0400

    exercice 2 - requis 4 fail

--- greeting.test.js
+++ greeting.test.js
@@ -1,12 +1,12 @@
 const greet = require('./greeting.js');
 
-describe("test greeting ", function () {
+describe("test greeting ", () => {
 	test('string is "Hello, Luke"', () => {
 		let name = "Luke";
 		expect(greet(name)).toBe("Hello, Luke");
 	});
 
-	describe('empty name returns "Hello there"', function () {
+	describe('empty name returns "Hello there"', () => {
 		test('undefined returns "Hello there"', () => {
 			let name;
 			expect(greet(name)).toBe("Hello there");
@@ -26,5 +26,10 @@ describe("test greeting ", function () {
 	test('uppercase name "HAN" returns "HELLO HAN"', () => {
 		let name = "HAN";
 		expect(greet(name)).toBe("HELLO HAN");
+	});
+
+	test('array of two names', () => {
+		let names = ["Jango", "Boba"];
+		expect(greet(names)).toBe("Hello, Jango and Boba");
 	});
 });
